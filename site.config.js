c1ff2dd16237e0a5d150fd8915c22949f980b6d0
kjkjkl <gqh2315@gmail.com>
2021-04-12 09:38:19 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -7,19 +7,19 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'kjkjkl',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+//   name: 'kjkjkl',
+//   domain: 'transitivebullsh.it',
+//   author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+//   description: 'Example site description',
+//   socialImageTitle: 'Transitive Bullshit',
+//   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'kjkjkl',
-  github: 'kjkjkl',
-  linkedin: 'kjkjkl',
+//   twitter: 'kjkjkl',
+//   github: 'kjkjkl',
+//   linkedin: 'kjkjkl',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
